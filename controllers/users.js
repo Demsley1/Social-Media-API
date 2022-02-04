c6d53b5804eace64cfa64e11b47bd3dda5784142
Denva <emsley3684@gmail.com>
2022-02-04 12:36:27 -0500

    new controller to handle delete of thoughts when user is deleted

--- controllers/users.js
+++ controllers/users.js
@@ -61,13 +61,14 @@ const userController = {
     // Delete a user
     deleteUser({ params }, res ){
         User.findOneAndDelete(
-            { _id: params.id },
-            { runValidators: true }
-        ).then(dbUserData => {
+            { _id: params.id }
+        ).then(({ thoughts: { _id } }) => {
+            return Thought.findByIdAndDelete({ _id: _id });
+        }).then(dbUserData => {
             if(!dbUserData){
                 return res.status(404).json({ message: 'No user found with this id!' });
             }
-            res.json(dbUserData);
+            res.json(dbUserData)
         }).catch(err => res.json(err));
     },
     
